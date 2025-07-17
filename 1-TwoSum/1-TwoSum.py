2be3ac313726c5113d86e50e15015dac3eed2f0d
Saksham Singh Malhotra <69833791+saksham-s1ngh@users.noreply.github.com>
2025-07-17 11:26:43 -0400

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,14 @@
+# Last updated: 17/07/2025, 11:26:42
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = {}
+        for i in range(len(nums)):
+            if target - nums[i] in num_dict:
+               return [num_dict[target - nums[i]], i]
+            num_dict[nums[i]] = i
+                
