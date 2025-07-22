123037135a52eb9a87d71632cce995ae016240d1
Saksham Singh Malhotra <69833791+saksham-s1ngh@users.noreply.github.com>
2025-07-22 12:11:05 -0400

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -0,0 +1,23 @@
+# Last updated: 22/07/2025, 12:11:05
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        # l1, l2 = len(str1), len(str2)
+        # while l1 % l2 != 0:
+        #     l1, l2 = l2, l1 % l2
+        
+        def gcd(l1, l2):
+            while l2 :
+                l1, l2 = l2, l1 % l2
+            return l1
+
+        return str1[:gcd(len(str1), len(str2))]
+        
+        
