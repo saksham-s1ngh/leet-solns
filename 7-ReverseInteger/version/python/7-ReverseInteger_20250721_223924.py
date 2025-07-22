216ac526bd2162e5c6d71ba18382328f12162b94
Saksham Singh Malhotra <69833791+saksham-s1ngh@users.noreply.github.com>
2025-07-21 22:39:25 -0400

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/version/python/7-ReverseInteger_20250721_223924.py
@@ -0,0 +1,21 @@
+# Last updated: 21/07/2025, 22:39:24
+from collections import deque
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        reversed_int = []
+        if x == 0: 
+            return 0
+        
+        if x < 0:
+            reversed_int.append("-")
+            x *= -1
+        while x > 0:
+            reversed_int.append(str(x%10))
+            x //= 10
+        
+        reversed_int = int("".join(reversed_int)) 
+        return reversed_int if -2**31 <= reversed_int <= 2**31 - 1 else 0
